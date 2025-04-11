8897a0b4c514825f5bdc4fa4a2b492335e6e98ff
NIU1705807 <1705807@uab.cat>
2025-04-11 11:50:34 +0200

    Classes Fitxa i moviment fetes

--- EL JOC DE LES DAMES/Fitxa.h
+++ EL JOC DE LES DAMES/Fitxa.h
@@ -1,25 +1,75 @@
-#pragma once
+#ifndef FITXA_H
+#define FITXA_H
 
-typedef enum
-{
-	TIPUS_NORMAL,
-	TIPUS_DAMA,
-	TIPUS_EMPTY,
+#include <iostream>
+#include "Moviment.h"
+
+const int MAX_MOVIMENTS = 20;
+
+typedef enum {
+    TIPUS_NORMAL,
+    TIPUS_DAMA,
+    TIPUS_EMPTY
 } TipusFitxa;
 
-typedef enum
-{
-	COLOR_NEGRE,
-	COLOR_BLANC,
+typedef enum {
+    COLOR_NEGRE,
+    COLOR_BLANC
 } ColorFitxa;
 
-class Fitxa
-{
-public:
-
-
+class Fitxa {
 private:
+    TipusFitxa tipus;
+    ColorFitxa color;
+    Moviment movimentsValids[MAX_MOVIMENTS];
+    int nMoviments;
 
+public:
+    Fitxa() : tipus(TIPUS_EMPTY), color(COLOR_BLANC), nMoviments(0) {}
 
+    Fitxa(TipusFitxa t, ColorFitxa c) : tipus(t), color(c), nMoviments(0) {}
+
+    TipusFitxa getTipus() const {
+        return tipus;
+    }
+
+    ColorFitxa getColor() const {
+        return color;
+    }
+
+    void convertirEnDama() {
+        if (tipus == TIPUS_NORMAL) {
+            tipus = TIPUS_DAMA;
+        }
+    }
+
+    void afegeixMovimentValid(const Moviment& m) {
+        if (nMoviments < MAX_MOVIMENTS) {
+            movimentsValids[nMoviments++] = m;
+        }
+    }
+
+    void netejaMoviments() {
+        nMoviments = 0;
+    }
+
+    int getNumMoviments() const {
+        return nMoviments;
+    }
+
+    Moviment getMoviment(int i) const {
+        return movimentsValids[i];
+    }
+
+    void mostraMoviments() const {
+        for (int i = 0; i < nMoviments; ++i) {
+            movimentsValids[i].mostra();
+        }
+    }
+
+    bool esBuida() const {
+        return tipus == TIPUS_EMPTY;
+    }
 };
 
+#endif
--- EL JOC DE LES DAMES/Moviment.h
+++ EL JOC DE LES DAMES/Moviment.h
@@ -1,9 +1,58 @@
-#pragma once
-class Moviment
-{
-public:
+#ifndef MOVIMENT_H
+#define MOVIMENT_H
 
+#include <iostream>
+#include "Posicio.h"
+
+const int MAX_POSICIONS = 12;
+
+class Moviment {
 private:
+    Posicio cami[MAX_POSICIONS];
+    int nPosicions;
+    int pecesMates;
 
+public:
+    Moviment() : nPosicions(0), pecesMates(0) {}
+
+    void afegeixPosicio(const Posicio& p) {
+        if (nPosicions < MAX_POSICIONS) {
+            cami[nPosicions++] = p;
+        }
+    }
+
+    void setPecesMates(int n) {
+        pecesMates = n;
+    }
+
+    int getPecesMates() const {
+        return pecesMates;
+    }
+
+    int getNumPosicions() const {
+        return nPosicions;
+    }
+
+    Posicio getPosicio(int i) const {
+        return cami[i];
+    }
+
+    Posicio getUltimaPosicio() const {
+        if (nPosicions == 0) return Posicio();
+        return cami[nPosicions - 1];
+    }
+
+    bool esMovimentValid() const {
+        return nPosicions > 0;
+    }
+
+    void mostra() const {
+        for (int i = 0; i < nPosicions; ++i) {
+            std::cout << cami[i].getPosicio();
+            if (i != nPosicions - 1) std::cout << " -> ";
+        }
+        std::cout << std::endl;
+    }
 };
 
+#endif
